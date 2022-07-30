115dd440f8c189e479824e660cea970fd62f3210
m4ndeokyi <askminjae@gmail.com>
2022-07-30 18:23:47 +0900

    Readjusted spectrum range

--- src/visualizer/CamelliaVisualizer.tsx
+++ src/visualizer/CamelliaVisualizer.tsx
@@ -362,7 +362,7 @@ const CamelliaVisualizer : React.FC<CamelliaVisualzerProps> = (props) => {
                 left={ windowSize.width / 2 - getRelative(44) }
                 width={ getRelative(777) }
                 bottom={ windowSize.height / 2 + getRelative(175) }
-                range={[ 0.002 / 2, 0.017 / 2 ]}
+                range={[ 0.0016 / 2, 0.016 / 2 ]}
                 waveScale={ getRelative(2.5) }
             />
             <ProgressBar
