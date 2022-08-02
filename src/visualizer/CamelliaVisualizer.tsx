b5136c9a7f49e3a61dae427a536c70177512e395
m4ndeokyi <askminjae@gmail.com>
2022-08-02 13:17:59 +0900

    Delete frameNumber in visualizer

--- src/visualizer/CamelliaVisualizer.tsx
+++ src/visualizer/CamelliaVisualizer.tsx
@@ -62,60 +62,34 @@ const CamelliaVisualizer : React.FC<CamelliaVisualzerProps> = (props) => {
     const [ volumeOnDisplay, setVolumeOnDisplay ] = useState<number>(0);
     const processedVolumeArray = useRef<Float32Array>();
 
-    const [ frameNumber, setFrameNumber ] = useState<number>(0);
     const [ imageSrc, setImageSrc ] = useState<string>();
 
 
     const calculationWorkerRef = useRef<FourierWorker.CustomFourierWorker>();
+    const playerRef = useRef<AudioPlayer>();
 
     const inputFileRef = useRef<HTMLInputElement>(null);
     const repeatCheckboxRef = useRef<HTMLInputElement>(null);
     const imageSrcInputRef = useRef<HTMLInputElement>(null);
 
-    const playerRef = useRef<AudioPlayer>();
     const processRef = useRef<number>(-1);
 
 
-    const start = useCallback((options: { frame?: number, forced?: boolean } = {}) => {
-        let { frame, forced } = options;
+    const start = useCallback((options: { seconds?: number, forced?: boolean } = {}) => {
+        let { seconds, forced } = options;
 
-        let player = playerRef.current;
-        if(!player) return;
-        
         if(processRef.current !== -1) return;
-        if(!player.isAudioInserted() || !parsedArray) return;
 
-        if(forced ? true : !player.isPlaying()) {
-            if(frame) {
-                player.start(frame / props.framerate);
-                setFrameNumber(frame);
-            }
-            else {
-                player.start(frameNumber / props.framerate);
-            }
-        }
-    }, [ frameNumber, parsedArray, props ])
-
-
-    const stop = useCallback((resetTimer: boolean = false) => {
         let player = playerRef.current;
-        if(!player) return;
-        
-        if(processRef.current === -1 && player.isAudioInserted()) {
-            player.stop();
-            if(resetTimer) setFrameNumber(0);
-        }
+        if(!player?.isAudioInserted() || !parsedArray) return;
+        if(forced ? true : !player.isPlaying()) player.start(seconds);
+    }, [ parsedArray ]);
+
+
+    const stop = useCallback((resetTime?: boolean) => {
+        if(processRef.current === -1 && playerRef.current?.isAudioInserted())
+            playerRef.current.stop(resetTime);
     }, []);
-
-
-    const setFrame = useCallback((frame: number) => {
-        let player = playerRef.current;
-        if(!player) return;
-
-        let playing = player.isPlaying();
-        stop();
-        playing ? start({ frame, forced: true }) : setFrameNumber(frame);
-    }, [ start, stop ])
 
 
     const updateProcessString = useCallback(() => {
@@ -127,63 +101,60 @@ const CamelliaVisualizer : React.FC<CamelliaVisualzerProps> = (props) => {
             setProcessingText(`Calculating: ${Math.trunc(processRef.current * 100) / 100}%`);
         }
     }, []);
+
+
+    const updateSpectrum = useCallback(() => {
+        let frame = Math.floor((playerRef.current?.getTime() ?? 0) * props.framerate);
+        let array = parsedArray?.[frame];
+        let mag = volumeArray?.[frame];
+        setArrayOnDisplay(array ?? emptyArrayOnDisplay);
+        setVolumeOnDisplay(mag ?? 0);
+    }, [ props, parsedArray, volumeArray ]);
     
 
-    const loop = useCallback((steps: number) => {
+    const loop = useCallback(() => {
         updateProcessString();
 
-        let newFrameNumber = frameNumber;
-        if(playerRef.current?.isPlaying()) newFrameNumber += steps;
-
-        if(parsedArray && newFrameNumber >= parsedArray.length) {
+        let time = (playerRef.current?.getTime() ?? 0) / 1000;
+        let frame = Math.floor(time * props.framerate)
+        if(parsedArray && frame >= parsedArray.length) {
             stop(true);
             if(repeatCheckboxRef.current?.checked) {
                 start({ forced: true });
             }
         }
-        else {
-            setFrameNumber(newFrameNumber);
-        }
-    }, [ start, stop, updateProcessString, parsedArray, frameNumber ])
 
-
-    const updateSpectrum = useCallback(() => {
-        let array = parsedArray?.[frameNumber];
-        let mag = volumeArray?.[frameNumber];
-        setArrayOnDisplay(array ?? emptyArrayOnDisplay);
-        setVolumeOnDisplay(mag ?? 0);
-    }, [ frameNumber, parsedArray, volumeArray ])
+        updateSpectrum();
+    }, [ props, start, stop, updateProcessString, updateSpectrum, parsedArray ])
 
 
     const onFileSelection = useCallback(async () => {
         processRef.current = 0;
         try {
             let player = playerRef.current;
-            if(!player) return;
+            if(!player) { processRef.current = -1; return; }
+
+            let worker = calculationWorkerRef.current;
+            if(!worker) { processRef.current = -1; return; }
 
             const inputFile = inputFileRef.current?.files?.[0];
-            if(!inputFile)  { processRef.current = -1; return; }
+            if(!inputFile) { processRef.current = -1; return; }
 
             let decoded = await player.insertAudioFile(inputFile);
             if(player.albumCoverUri) setImageSrc(player.albumCoverUri);
 
             let { numberOfChannels, sampleRate, length } = decoded;
-
             const channelsData = new Array(numberOfChannels).fill(0).map((_, i) => decoded.getChannelData(i));
 
-            if(calculationWorkerRef.current) {
-                // TODO: figure out whether to make the sample rate customizable
-                calculationWorkerRef.current.postMessage({
-                    type: 'input',
-                    dataArrayLength: length,
-                    channelsData, sampleRate,
-                    framerate: props.framerate,
-                    transformZoom: 4, // Change this property's name
-                    customSampleRate: 2048
-                })
-            } else {
-                processRef.current = -1;
-            }
+            // TODO: figure out whether to make the sample rate customizable
+            worker.postMessage({
+                type: 'input',
+                dataArrayLength: length,
+                channelsData, sampleRate,
+                framerate: props.framerate,
+                transformZoom: 4, // Change this property's name
+                customSampleRate: 2048
+            })
         } catch(e) {
             console.error(e);
             processRef.current = -1;
@@ -198,7 +169,7 @@ const CamelliaVisualizer : React.FC<CamelliaVisualzerProps> = (props) => {
 
     const onProgressBarClick : ProgressBarClickHandler = (value) => {
         if(playerRef.current?.isAudioInserted() && parsedArray) {
-            setFrame(Math.floor(value));
+            playerRef.current.setTime(Math.floor(value), true);
         }
     }
 
@@ -332,15 +303,15 @@ const CamelliaVisualizer : React.FC<CamelliaVisualzerProps> = (props) => {
                 width={ getRelative(777) }
                 bottom={ windowSize.height / 2 + getRelative(175) }
                 range={[ 0.0016 / 2, 0.017 / 2 ]}
-                waveScale={ getRelative(2) }
+                waveScale={ getRelative(2.5) }
             />
             <ProgressBar
                 color={ mergeColor(props.progressBar, props.defaultColor) }
                 left={ windowSize.width / 2 - getRelative(40) }
                 width={ getRelative(776) }
                 y={ windowSize.height / 2 + getRelative(194) }
-                current={ frameNumber }
-                total={ parsedArray?.length ?? 0 }
+                current={ playerRef.current?.getTime() ?? 0 }
+                total={ (parsedArray?.length ?? 0) / props.framerate }
                 ballRadius={ getRelative(8) }
                 onClick={ onProgressBarClick }
             />
--- src/visualizer/audio/AudioPlayer.tsx
+++ src/visualizer/audio/AudioPlayer.tsx
@@ -6,11 +6,17 @@ export class AudioPlayer {
     buffer?: AudioBuffer;
     bufferSource?: AudioBufferSourceNode;
     private playing: boolean;
+    /** In seconds. */
+    private startTime: number;
+    /** In seconds. */
+    private time: number;
 
 
     constructor() {
         this.context = new AudioContext();
         this.playing = false;
+        this.startTime = this.getNow();
+        this.time = 0;
     }
 
 
@@ -21,6 +27,7 @@ export class AudioPlayer {
 
         const inputBuffer = await input.arrayBuffer();
         if(!inputBuffer) throw new Error('Empty file array buffer');
+
         return this.insertAudioBuffer(inputBuffer);
     }
 
@@ -46,40 +53,103 @@ export class AudioPlayer {
     }
 
 
-    start(seconds: number) {
+    start(seconds?: number) {
         if(!this.buffer) throw new Error('Audio not inserted');
 
         const bufferSource = this.context.createBufferSource();
         bufferSource.buffer = this.buffer;
         bufferSource.connect(this.context.destination);
 
-        bufferSource.start(0, seconds);
+        let now = this.getNow();
+        if(seconds) {
+            bufferSource.start(0, seconds);
+            this.startTime = now - seconds;
+        }
+        else {
+            bufferSource.start(0, this.time);
+            this.startTime = now - this.time;
+        }
         this.bufferSource = bufferSource;
         this.playing = true;
     }
 
 
-    stop() {
+    stop(resetTime? : boolean) {
         if(!this.buffer) throw new Error('Audio not inserted');
 
         this.bufferSource?.stop();
         this.playing = false;
-    }
-    
 
-    async extractAlbumCover(file: File) : Promise<string> {
-        return new Promise<string>((res, rej) => {
+        if(resetTime) {
+            this.time = 0;
+        } else {
+            let now = this.getNow();
+            this.time = now - this.startTime;
+        }
+    }
+
+
+    setTime(seconds: number, replayIfPlaying?: boolean) {
+        let playing = this.playing;
+        this.stop();
+        if(playing && replayIfPlaying) this.start(seconds);
+        else this.time = seconds;
+    }
+
+
+    /** In seconds. */
+    getTime() {
+        this.updateTime();
+        return this.time;
+    }
+
+
+    /** In seconds. */
+    getDuration() {
+        if(!this.buffer) throw new Error('Audio not inserted');
+        return this.buffer.duration;
+    }
+
+
+    /** In seconds. */
+    private getNow() {
+        return new Date().getTime() / 1000;
+    }
+
+
+    private updateTime() {
+        if(!this.buffer) return;
+
+        let now = new Date().getTime() / 1000;
+        if(this.playing) {
+            this.time = now - this.startTime;
+        } else {
+            this.startTime = now - this.time;
+        }
+
+        if(this.time > this.buffer.duration) {
+            this.playing = false;
+            this.time = 0;
+            this.startTime = now;
+        }
+    }
+
+
+    async extractAlbumCover(file: File) : Promise<string | undefined> {
+        return new Promise<string | undefined>((res) => {
             window.jsmediatags.read(file, {
                 onSuccess: ({ tags }) => {
                     let { picture } = tags;
-                    if(!picture) return;
+                    if(!picture) {
+                        res(undefined);
+                        return;
+                    }
     
                     let base64String = picture.data.map((c) => String.fromCharCode(c)).reduce((prev, cur) => prev + cur);
                     let imageUri = "data:" + picture.format + ";base64," + window.btoa(base64String);
-    
                     res(imageUri);
                 },
-                onError: rej
+                onError: () => res(undefined)
             })
         })
     }
